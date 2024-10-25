c7b35b0724c6b0cdc65cdd1a9ef7f0570cee1a2d
rima2787 <27rima25@gmail.com>
2024-10-25 23:19:16 +0600

    added new file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Blogs from './componenets/Blogs/Blogs'
+import Header from './componenets/Header/Header'
+function App() {
+  
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
